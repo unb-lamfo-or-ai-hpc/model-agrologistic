a02259e2bc0810f1b58d14b8d44bbd512c5d021c
Victor Rafael R Celestino <cvictor@uol.com.br>
2026-08-24 14:52:02 -0300

    Removendo interface grafica, arquivos de servidor e conteinerizacao

--- run_server.py
+++
@@ -1,2 +0,0 @@
-from src.view.view import app
-app.run(debug=True, port=8050)
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-from src.view.view import app
-
-# O Gunicorn procura por uma variável chamada 'application' ou 'server' por padrão
-server = app.server
